32118129c302390b1b9e3eaeb0dcdd2294689d04
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-05-24 15:02:47 -0600

    E2E Tests for Auth

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import { NestFactory } from '@nestjs/core'
-import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
-// import { CookieSession } from 'cookie-session'
-const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.use(
-    cookieSession({
-      keys: ['asdfghj'],
-    }),
-  )
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  )
+
   await app.listen(3000)
 }
 bootstrap()
